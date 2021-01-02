2de0ba5f0310c295886d4a0c96971c7e5602ce9f
Alexander Bukhalo <a@bukhalo.com>
2021-01-02 04:06:54 +0300

    feat(): custom telegram bot api url support

--- src/interfaces/telegram-module-options.interface.ts
+++ src/interfaces/telegram-module-options.interface.ts
@@ -2,6 +2,12 @@ import { ModuleMetadata, Type } from '@nestjs/common/interfaces';
 
 export interface TelegramModuleOptions {
   botKey: string;
+  /**
+   * Change API url for self-hosted telegram bot api server.
+   * @see https://core.telegram.org/bots/api#using-a-local-bot-api-server
+   * @example https://domain.tld
+   */
+  apiUrl?: string;
 }
 
 export interface TelegramOptionsFactory {
